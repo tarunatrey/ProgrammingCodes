4183eaed99138166c7d17c5dcb317ce6e7c17a26
Tarun Atrey <tarun@ciinow.com>
2014-03-30 01:55:57 -0700

    Print all subsets of an int array with 0 sum
    > Create a sumarray for each element that is equal to sum of all the
    elements till that point.
    > Check for duplicate values in this sumarray, all the elements between
    them will sum to 0
    > It works for overlapping intervals too

---
+++ PrintZeroSumSubsets.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+
+using namespace std;
+
+int arr[]={1,2,3,-2,-1,4,-5,1};
+
+int main()
+{
+    int iNumOfElements;
+    iNumOfElements = sizeof(arr)/sizeof(int);
+    
+    int *sumarr = new int[iNumOfElements*sizeof(int)];
+    for(int i=0; i<iNumOfElements; i++)
+    {
+        if(i==0)
+	{
+            sumarr[i] = arr[i];
+	}
+        else
+	{
+            sumarr[i] = sumarr[i-1] + arr[i];
+	}
+    }
+
+    cout<<"\n Original array : ";
+    for(int i=0; i<iNumOfElements; i++)
+    {
+        cout<<" "<<arr[i];
+    }
+
+    cout<<"\n Calculated sum array : ";
+    for(int i=0; i<iNumOfElements; i++)
+    {
+        cout<<" "<<sumarr[i];
+    }
+
+    cout<<"\n Subsets with sum zero :-";
+    for(int i=0; i<iNumOfElements-1; i++)
+    {
+        for(int j=i+1; j<iNumOfElements; j++)
+        {
+            if(sumarr[i] == sumarr[j])
+            {
+                cout<<"\n";
+                for(int k=i+1; k<=j; k++)
+                    cout<<" "<<arr[k];
+            }  
+        }
+    }
+    cout<<"\n";
+
+    return 0;
+}
