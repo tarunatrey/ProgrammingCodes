c70a5f821633c52cfaf944cadf5589aaafb4b749
Tarun Atrey <tarunatrey@gmail.com>
2014-05-22 14:46:16 -0700

    Bug fix : checking length doesn't matter
    Length has been modified one step above. Also some indentation changes.

--- RotationStringsCheck.cpp
+++ RotationStringsCheck.cpp
@@ -1,3 +1,11 @@
+/////////////////////////////////
+// Author     : Tarun Atrey
+// Date       : May 2014
+// Copyright  : 2014 - Present
+// Notes      : Check if two strings are rotations of each other
+// Complexity : O(N)
+/////////////////////////////////
+
 #include<iostream>
 #include<string>
 
@@ -5,17 +13,20 @@ using namespace std;
 
 int main()
 {
-    	string str1;
-    	string str2;
+   	string str1;
+   	string str2;
+
 	cout<<"\n Enter string 1 : ";
 	cin>>str1;
 	cout<<"\n Enter string 2 : ";
 	cin>>str2;
-        str1 = str1 + str1;
+    
+    str1 = str1 + str1;
         
-        if (str1.find(str2) != string::npos && str1.length() == str2.length())
+    if (str1.find(str2) != string::npos)
 		cout<<"\n Given strings are rotations of each other!\n";
-        else
-            	cout<<"\n Given strings are not rotations!\n";
-    	return 0;
+    else
+        cout<<"\n Given strings are not rotations!\n";
+    
+    return 0;
 }
